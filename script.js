89aadf95086d64eb2e72312a58a74a57cabff780
zuhra <zuhraaldaiarova@icloud.com>
2025-11-11 19:11:46 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+let sum1=document.getElementById("sum1")
+
+let sum2=document.getElementById("sum2")
+
+let result=document.getElementById("result")
+
+
+
+const Plus=()=>{
+    result.innerHTML=Number(sum1.value)+Number(sum2.value)
+    
+}
+
+const minus=()=>{
+    result.innerHTML=Number(sum1.value)-Number(sum2.value)
+    
+}
+const coboityy=()=>{
+    result.innerHTML=Number(sum1.value)*Number(sum2.value)
+    
+}
+const boluu=()=>{
+    result.innerHTML=Number(sum1.value)/Number(sum2.value)
+    
+}
